6239081ebd9776383714e8d3fff36dd9150bcd4b
Hp <Hp@LAPTOP-V2PDL8GR>
2020-09-19 14:53:51 +0530

    generic repository pattern

---
+++ generic repository pattern/Controllers/EmployeeController.cs
@@ -0,0 +1,104 @@
+﻿using generic_repository_pattern.Models;
+using generic_repository_pattern.Models.DAL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace generic_repository_pattern.Controllers
+{
+    public class EmployeeController : Controller,IDisposable
+    {
+        private _IAllRepository<EmployeeMaster> interfaceobj;
+        public EmployeeController()
+        {
+            this.interfaceobj = new AllRepository<EmployeeMaster>();
+        }
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View(from m in interfaceobj.GetModel() select m);
+        }
+
+        // GET: MyBooks/Details/5
+        public ActionResult Details(int id)
+        {
+            EmployeeMaster b = interfaceobj.GetModelByID(id);
+            return View(b);
+        }
+
+        // GET: MyBooks/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: MyBooks/Create
+        [HttpPost]
+        public ActionResult Create(EmployeeMaster collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+                interfaceobj.InsertModel(collection);
+                interfaceobj.Save();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MyBooks/Edit/5
+        public ActionResult Edit(int id)
+        {
+            EmployeeMaster b = interfaceobj.GetModelByID(id);
+            return View(b);
+        }
+
+        // POST: MyBooks/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, EmployeeMaster collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                interfaceobj.UpdateModel(collection);
+                interfaceobj.Save();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: MyBooks/Delete/5
+        public ActionResult Delete(int id)
+        {
+
+            EmployeeMaster b = interfaceobj.GetModelByID(id);
+            return View(b);
+        }
+
+        // POST: MyBooks/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, EmployeeMaster collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                interfaceobj.DeleteModel(id);
+                interfaceobj.Save();
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        
+    }
+}
---
+++ generic repository pattern/Models/DAL/AllRepository.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+
+namespace generic_repository_pattern.Models.DAL
+{
+    public class AllRepository<T> : _IAllRepository<T> where T:class{
+        private webapiEntities _context;
+        private IDbSet<T> dbEntity;
+
+        public AllRepository()
+        {
+            _context = new webapiEntities();
+            dbEntity = _context.Set<T>();
+        }
+
+        public void DeleteModel(int modelID)
+        {
+            T model = dbEntity.Find(modelID);
+            dbEntity.Remove(model);
+        }
+
+        public IEnumerable<T> GetModel()
+        {
+            return dbEntity.ToList();
+        }
+
+        public T GetModelByID(int modelId)
+        {
+            return dbEntity.Find(modelId);
+        }
+
+        public void InsertModel(T model)
+        {
+            dbEntity.Add(model);
+        }
+
+        public void Save()
+        {
+            _context.SaveChanges();
+        }
+
+        public void UpdateModel(T model)
+        {
+            _context.Entry(model).State = System.Data.Entity.EntityState.Modified;
+        }
+    }
+
+}
